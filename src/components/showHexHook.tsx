38cdb0956e56c3f6944b8c5e4640a4e0717b6cff
Matt Russell <MattRussellUK@gmail.com>
2024-05-21 18:25:10 +0100

    rename dist -> build

--- src/components/showHexHook.tsx
+++ src/components/showHexHook.tsx
@@ -2,17 +2,18 @@ import React, { ReactNode, useContext, useState } from 'react'
 
 export interface ShowHexContextContents {
   showHex: boolean
+
   setShowHex(b: boolean): void
 }
 
 const ShowHexContext = React.createContext<ShowHexContextContents>({
   showHex: false,
-  setShowHex: () => undefined,
+  setShowHex: () => undefined
 })
 
 export const useShowHex = (): ShowHexContextContents => useContext(ShowHexContext)
 
-export const ShowHexProvider = ({ children }: {children?: ReactNode | undefined}) => {
+export const ShowHexProvider = ({ children }: { children?: ReactNode | undefined }) => {
   const [showHex, setShowHex] = useState<boolean>(false)
 
   return <ShowHexContext.Provider value={{ showHex, setShowHex }}>{children}</ShowHexContext.Provider>
